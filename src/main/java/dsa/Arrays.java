7671bb83a1a7a19346dee0dad1f415f9a02560fa
Mosima-M <311113194+Mosima-M@users.noreply.github.com>
2026-08-03 19:08:52 +0200

    add max profit and twosums

--- src/main/java/dsa/Arrays.java
+++ src/main/java/dsa/Arrays.java
@@ -1,5 +1,8 @@
 package dsa;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Arrays {
     // Question 1 — Two Sum
     // Given an array of integers and a target, return the indices of the
@@ -7,9 +10,28 @@ public class Arrays {
     public static int[] twoSum(int[] nums, int target) {
 
         // TODO: implement
-        return null;
+        for(int i = 0; i < nums.length; i++){
+            for(int y = i +1; y < nums.length; y++){
+                if(nums[i] + nums[y] == target){
+                    return new int[]{i,y};
+                }
+            }
+        }
+        return new int[0];
     }
 
+    public static int[] twoSumHashMap(int[] nums, int target) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if (map.containsKey(diff)) {
+                return new int[]{map.get(diff), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[0];
+    }
 
     // Question 2 — Best Time to Buy and Sell Stock
     // prices[i] is the price on day i. Return the maximum profit from
@@ -18,8 +40,16 @@ public class Arrays {
     public static int maxProfit(int[] prices) {
 
         // TODO: implement
-
-        return 0;
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                int profit = prices[j] - prices[i];
+                if (profit > maxProfit) {
+                    maxProfit = profit;
+                }
+            }
+        }
+        return maxProfit;
     }
 
     // Question 4 — Product of Array Except Self
