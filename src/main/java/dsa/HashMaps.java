3348a7e321ad853ed1780755dc572146ce29f700
Mosima-M <311113194+Mosima-M@users.noreply.github.com>
2026-08-05 23:40:00 +0200

    add: hashmapchallenge

--- src/main/java/dsa/HashMaps.java
+++ src/main/java/dsa/HashMaps.java
@@ -1,4 +1,20 @@
 package dsa;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class HashMaps {
+
+    public static HashMap<String, Integer> countWords(ArrayList<String> words){
+        return null;
+    }
+
+    public static String highestMark(HashMap<String, Integer> marks){
+        return null;
+    }
+
+    public static HashMap<String, Integer> removeBelow(HashMap<String, Integer> map, int minimum){
+        return null;
+    }
 }
+
