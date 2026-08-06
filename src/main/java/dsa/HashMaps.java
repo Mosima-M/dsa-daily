f1272c74f2302fc10cb5901b5bfedd961e74c067
Mosima-M <311113194+Mosima-M@users.noreply.github.com>
2026-08-06 23:33:10 +0200

    count words solution

--- src/main/java/dsa/HashMaps.java
+++ src/main/java/dsa/HashMaps.java
@@ -5,8 +5,18 @@ import java.util.HashMap;
 
 public class HashMaps {
 
-    public static HashMap<String, Integer> countWords(ArrayList<String> words){
-        return null;
+    public static HashMap<String, Integer> countWords(ArrayList<String> words) {
+
+        HashMap<String, Integer> wordCount = new HashMap<>();
+
+        for (String word : words) {
+            if (wordCount.containsKey(word)) {
+                wordCount.put(word, wordCount.get(word) + 1);
+            } else {
+                wordCount.put(word, 1);
+            }
+        }
+        return wordCount;
     }
 
     public static String highestMark(HashMap<String, Integer> marks){
