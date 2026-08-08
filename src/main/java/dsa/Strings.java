6ff3bcbba4d801a03f363d852f7ad9bba1d0edae
Mosima-M <311113194+Mosima-M@users.noreply.github.com>
2026-08-08 23:27:25 +0200

    add dsa strings

--- src/main/java/dsa/Strings.java
+++ src/main/java/dsa/Strings.java
@@ -1,4 +1,15 @@
 package dsa;
 
 public class Strings {
+
+    public static String reverseString(String text){
+        return "";
+    }
+    public static int countVowels(String text){
+        return 0;
+    }
+
+    public static boolean isPalindrome(String text){
+        return false;
+    }
 }
