423a73bdac293864f03cda587def7d7077343146
Mosima-M <311113194+Mosima-M@users.noreply.github.com>
2026-08-08 23:32:54 +0200

    add count vowels tests

--- src/test/java/dsa/StringsTests.java
+++ src/test/java/dsa/StringsTests.java
@@ -11,5 +11,13 @@ class StringsTests {
         assertEquals("321", Strings.reverseString("123"));
         assertEquals("", Strings.reverseString(""));
     }
+    @Test
+    void countVowelsTests() {
+        assertEquals(2, Strings.countVowels("hello"));
+        assertEquals(5, Strings.countVowels("AEIOU"));
+        assertEquals(0, Strings.countVowels("rhythm"));
+        assertEquals(3, Strings.countVowels("Java Programming"));
+        assertEquals(0, Strings.countVowels(""));
+    }
 
 }
