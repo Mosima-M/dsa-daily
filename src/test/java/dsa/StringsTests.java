e26aeacb8f76a2c8f4b4a4bb41ca2ae25c20226e
Mosima-M <311113194+Mosima-M@users.noreply.github.com>
2026-08-08 23:32:21 +0200

    add reverse string tests

--- src/test/java/dsa/StringsTests.java
+++ src/test/java/dsa/StringsTests.java
@@ -1,4 +1,15 @@
 package dsa;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class StringsTests {
+class StringsTests {
+
+    @Test
+    void reverseStringTests() {
+        assertEquals("olleh", Strings.reverseString("hello"));
+        assertEquals("avaJ", Strings.reverseString("Java"));
+        assertEquals("321", Strings.reverseString("123"));
+        assertEquals("", Strings.reverseString(""));
+    }
+
 }
